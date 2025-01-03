54c00f2407b10f19fb003c5be86caaf696da859d
Dominik Lust <lust20035@hs-ansbach.de>
2025-01-03 13:48:21 +0100

    Updated Proect
    - Added extra Scenes to automatically being reset when teleported through the door without needing to complicatedly make a resetmechanic

---
+++ VR Room/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+    public void LoadUebung()
+    {
+        SceneManager.LoadScene("PA_Übungsraum");
+    }
+
+    public void LoadPruefung()
+    {
+        SceneManager.LoadScene("PA_Prüfraum");
+    }
+
+    public void LoadIntro()
+    {
+        SceneManager.LoadScene("PA_Intro");
+    }
+}
